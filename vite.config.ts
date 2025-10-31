0912137f1082bc213be8a467a238bcd13f6c171d
itsabhishekm <mabhishek2817@gmail.com>
2025-10-30 20:34:31 -0700

    updated the vite file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio_Abhishek/",
+  base: "./",   // <- change this to a relative base
   plugins: [react()],
 })
